a144a79c685ab5627af246937db2d71eec84113e
D4ker <Alex-Daker@yandex.ru>
2019-04-21 01:54:24 +0300

    Added files for calc functions

---
+++ src/js/func/karnaughMap/karnaughMap.js
@@ -0,0 +1,10 @@
+// Код, реализующий минимизацию картами Карно на основе массива Y[]
+
+// Строит карты Карно на основе массива Y[]
+function createKarnaughMap(string) {
+	let truthArray = getTruthTable(string); // Получить Y[]
+
+	// КОД
+};
+
+// КОД
